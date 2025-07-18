4f15cff82c0a597edeb9852c3fdadd79c37355a3
daey <kodhyoung__@naver.com>
2025-07-18 18:22:05 +0900

    proxy commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function Login() {
+    const User = {
+        email: 'abc@naver.com',
+        pw: 'qwe123'
+    };
+    const [userid, setUserid] = useState('');
+    const [pwd, setPwd] = useState('');
+
     return (
         <div>
-            <h1>로그인페이지 입니다.</h1>
+            <form onSubmit={async (event) => {
+                event.preventDefault();
+                const response = await fetch('/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        userid: userid,
+                        password: pwd
+                    })
+                });
+                if (response.ok) {
+                    alert('로그인 성공!');
+                } else {
+                    alert('로그인 실패!');
+                }
+            }}>
+                <p>id <input type="text" name="id" value={userid} onChange={e => setUserid(e.target.value)} /></p>
+                <p>pwd <input type="password" name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} /></p>
+                <p><input type="submit" value="로그인"></input></p>
+            </form>
         </div>
     );
 }
