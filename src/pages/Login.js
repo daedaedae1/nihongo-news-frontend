5bbc36dd6683c93d80082188c05553936408da5a
daey <kodhyoung__@naver.com>
2025-07-24 21:02:10 +0900

    form css commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,35 @@ function Login({ setUserInfo }) {
     const [pwd, setPwd] = useState('');
 
     return (
-        <div>
+        // <div>
+        //     <form onSubmit={async (event) => {
+        //         event.preventDefault();
+        //         const response = await fetch('http://localhost:8080/api/login', {
+        //             method: 'POST',
+        //             credentials: 'include',
+        //             headers: {
+        //                 'Content-Type': 'application/json'
+        //             },
+        //             body: JSON.stringify({
+        //                 userid: userid,
+        //                 password: pwd
+        //             }),
+        //         });
+        //         const data = await response.json();
+        //         if (response.ok) {
+        //             alert(data.success);
+        //             setUserInfo(data);
+        //             navigate('/');
+        //         } else {
+        //             alert(data.error);
+        //         }
+        //     }}>
+        //         <p>아이디 <input type="text" name="id" value={userid} onChange={e => setUserid(e.target.value)} /></p>
+        //         <p>비밀번호 <input type="password" name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} /></p>
+        //         <p><input type="submit" value="로그인"></input></p>
+        //     </form>
+        // </div>
+        <div className='container mt-5 pt-5 pb-5' style={{backgroundColor: 'white'}}>
             <form onSubmit={async (event) => {
                 event.preventDefault();
                 const response = await fetch('http://localhost:8080/api/login', {
@@ -32,9 +60,21 @@ function Login({ setUserInfo }) {
                     alert(data.error);
                 }
             }}>
-                <p>아이디 <input type="text" name="id" value={userid} onChange={e => setUserid(e.target.value)} /></p>
-                <p>비밀번호 <input type="password" name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} /></p>
-                <p><input type="submit" value="로그인"></input></p>
+                <div className='row mb-3 justify-content-center'>
+                    <label htmlFor='userid' className='col-sm-2 col-form-label fw-semibold'>아이디</label>
+                    <div className='col-md-4 '>
+                        <input type='text' className='form-control' id='userid' name="id" value={userid} onChange={e => {setUserid(e.target.value)}} />
+                    </div>
+                </div>
+                <div className='row mb-3 justify-content-center'>
+                    <label htmlFor='password' className='col-sm-2 col-form-label fw-semibold'>비밀번호</label>
+                    <div className='col-md-4'>
+                        <input type='password' className='form-control' id='password' name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} />
+                    </div>
+                </div>
+                <div className='d-grid gap-2 col-4 mt-4 mx-auto'>
+                    <input type="submit" className='btn btn-primary' value="로그인"></input>
+                </div>
             </form>
         </div>
     );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -26,7 +26,7 @@ function Signup() {
     }
 
     return (
-        <div>
+       <div className='container mt-5 pt-5 pb-5' style={{backgroundColor: 'white'}}>
             <form onSubmit={async (event) => {
                 event.preventDefault();
                 const response = await fetch('http://localhost:8080/api/signup', {
@@ -49,12 +49,36 @@ function Signup() {
                     alert(messageData.error);
                 }
             }}>
-                <p>이름 <input type="text" name="name" value={name} onChange={e => setName(e.target.value)}/></p>
-                <p>닉네임 <input type="text" name="nickname" value={nickname} onChange={e => setNickname(e.target.value)} /></p>
-                <p>아이디 <input type="text" name="id" value={userid} onChange={e => {setUserid(e.target.value); setIsAvailable(null);}}  />
-                          <button type="button" onClick={handleCheckId} disabled={!userid}>중복확인</button></p>
-                <p>비밀번호 <input type="password" name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} /></p>
-                <p><input type="submit" value="회원가입" disabled={isAvailable !== true}></input></p>
+                <div className='row mb-3 justify-content-center'>
+                    <label htmlFor='userid' className='col-sm-2 col-form-label fw-semibold'>아이디</label>
+                    <div className='col-md-4 '>
+                        <div className='input-group'>
+                            <input type='text' className='form-control' id='userid' name="id" value={userid} onChange={e => {setUserid(e.target.value); setIsAvailable(null);}} />
+                            <button type="button" className='btn btn-outline-secondary' onClick={handleCheckId} disabled={!userid} style={{color: '#000000'}}>중복확인</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='row mb-3 justify-content-center'>
+                    <label htmlFor='password' className='col-sm-2 col-form-label fw-semibold'>비밀번호</label>
+                    <div className='col-md-4'>
+                        <input type='password' className='form-control' id='password' name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} />
+                    </div>
+                </div>
+                <div className='row mb-3 justify-content-center'>
+                    <label htmlFor='nickname' className='col-sm-2 col-form-label fw-semibold'>닉네임</label>
+                    <div className='col-md-4'>
+                        <input type='text' className='form-control' id='nickname' name="nickname" value={nickname} onChange={e => setNickname(e.target.value)} />
+                    </div>
+                </div>
+                <div className='row mb-3 justify-content-center'>
+                    <label htmlFor='name' className='col-sm-2 col-form-label fw-semibold'>이름</label>
+                    <div className='col-md-4'>
+                        <input type='text' className='form-control' id='name' name="name" value={name} onChange={e => setName(e.target.value)} />
+                    </div>
+                </div>
+                <div className='d-grid gap-2 col-4 mt-4 mx-auto'>
+                    <input type="submit" className='btn btn-primary' value="회원가입" disabled={isAvailable !== true}></input>
+                </div>
             </form>
         </div>
     );
