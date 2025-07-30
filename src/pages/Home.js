3b672fb88593fdf38befd7678ef3666ef9c47c5d
daey <kodhyoung__@naver.com>
2025-07-30 18:32:40 +0900

    crawler test commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
+import { useState, useEffect } from 'react';
+
 function Home({ userInfo }) {
+
+  const [newsList, setNewsList] = useState([]);
+
+  const fetchNewsList = async () => {
+    const response = await fetch('http://localhost:8080/api/news/list');
+    const data = await response.json(); 
+    console.log('받아온 데이터', data);
+    setNewsList(data);
+  }
+
+  useEffect(() => {
+    fetchNewsList();
+  }, []);
+
   return (
     <div>
       {userInfo? (
         <>
           <h1>로그인된 홈입니다.</h1>
+          {newsList.map((news, idx) => (
+            <ul>
+              <li key={idx}>
+                <h3>{news.title}</h3>
+                <a href={news.url} target="_blank" rel="noopener noreferrer">{news.url}</a><br />
+                <img src={news.image} alt={news.title} width={100} /><br />
+                <span>{news.date}</span>
+              </li>
+            </ul>
+          ))}
         </>
       ) : (
         <>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,34 +9,6 @@ function Login({ setUserInfo }) {
     const [pwd, setPwd] = useState('');
 
     return (
-        // <div>
-        //     <form onSubmit={async (event) => {
-        //         event.preventDefault();
-        //         const response = await fetch('http://localhost:8080/api/login', {
-        //             method: 'POST',
-        //             credentials: 'include',
-        //             headers: {
-        //                 'Content-Type': 'application/json'
-        //             },
-        //             body: JSON.stringify({
-        //                 userid: userid,
-        //                 password: pwd
-        //             }),
-        //         });
-        //         const data = await response.json();
-        //         if (response.ok) {
-        //             alert(data.success);
-        //             setUserInfo(data);
-        //             navigate('/');
-        //         } else {
-        //             alert(data.error);
-        //         }
-        //     }}>
-        //         <p>아이디 <input type="text" name="id" value={userid} onChange={e => setUserid(e.target.value)} /></p>
-        //         <p>비밀번호 <input type="password" name="pwd" value={pwd} onChange={e => setPwd(e.target.value)} /></p>
-        //         <p><input type="submit" value="로그인"></input></p>
-        //     </form>
-        // </div>
         <div className='container mt-5 pt-5 pb-5' style={{backgroundColor: 'white'}}>
             <form onSubmit={async (event) => {
                 event.preventDefault();
